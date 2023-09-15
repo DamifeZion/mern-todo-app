0b9eefff1f30f1a5a810f82d09c96b49188d1153
Damife Zion <damifeolaleye@gmail.com>
2023-09-15 12:28:43 +0100

    minor update

--- client/src/middleWares/loginMiddleware/loginMiddleWare.js
+++ client/src/middleWares/loginMiddleware/loginMiddleWare.js
@@ -18,7 +18,7 @@ const loginMiddleWare = {
     try {
       disableButton();
       dispatch(loginSlice.actions.setIsLoading(true));
-      const url = "http://localhost:5000/api/user/login";
+      const url = "https://taskflow-crud.onrender.com/user/login";
       const res = await fetch(url, {
         method: "POST",
         body: JSON.stringify(formData),
